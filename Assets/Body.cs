ffc0836ae4b96323ec7b0435065561626d82e207
Jaco8485 <jaco8485@gmail.com>
2020-02-29 12:56:58 +0100

    fix jacobplayer

---
+++ Assets/Body.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform head;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rot = new Quaternion(0,head.rotation.y,0,head.rotation.w);
+        transform.rotation = rot;
+    }
+}
