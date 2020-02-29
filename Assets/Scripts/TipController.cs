56e80116a3ab24f4b9a670f8b841722d86072588
RedYodaDK <stigfrederiksen32@gmail.com>
2020-02-29 01:09:48 +0100

    Change to test, and changes to new edition of the hacker stuff

---
+++ Assets/Scripts/TipController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipController : MonoBehaviour
+{
+    public GameObject tipPosition;
+    public List<Tip> tipList;
+    private int currentTip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class Tip
+{
+    public string Hedder { get; set; }
+    public string Tips { get; set; }
+
+    public Tip(string hedder, string tips)
+    {
+        Hedder = hedder;
+        Tips = tips;
+    }
+}
