3268c8492821862c78c8d25bd666f79ea56841fe
Jaco8485 <jaco8485@gmail.com>
2020-03-01 02:35:08 +0100

    Level 2 done (ikke testedet)

---
+++ Assets/Scripts/TPElevator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPElevator : MonoBehaviour
+{
+    public Transform tpTarget;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.transform.position = tpTarget.position;
+        }
+    }
+
+}
